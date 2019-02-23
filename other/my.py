5361d650f4643cfbcb3e65035f86ec82bac399cb
efekta <efekta9@gmail.com>
2019-02-23 20:18:10 +0300

    tceh and stepic (other)

---
+++ other/my.py
@@ -0,0 +1,10 @@
+def convert():
+    try:
+        c = input('Введите число: ')
+        return float(с)
+    except ValueError:
+        print('Не удалось преобразовать строку в число с плавающей точкой.')
+    result = c
+    print(result)
+
+convert()
---
+++ other/task.py
@@ -0,0 +1,25 @@
+# 1) Напишите программу, которая считает площадь прямоугольника, спрашивая у пользователя длину двух сторон
+# def func_s():
+#     a = int(input('Введите длинну одной стороны прямоугольника: '))
+#     b = int(input('Введите длинну другой стороны прямоугольника: '))
+#     return a * b
+# result = func_s()
+# print(result)
+#
+# func_s()
+
+# 2) Напишите программу, которая спрашивает у пользователя два числа и знак: "+" или "-".
+# В зависимости от знака выводит их сумму или разницу.
+# def func_numbers():
+#     number_one = int(input('Введите число: '))
+#     number_two = int(input('Введите еще число: '))
+#     result = input('Введите знак + или - : ')
+#     if result == '+':
+#         amount = number_one + number_two
+#         print(amount)
+#     elif result == '-':
+#         difference = number_one - number_two
+#         print(difference)
+#
+# func_numbers()
+
