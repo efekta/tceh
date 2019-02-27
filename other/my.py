cc79a9393a4de88452392fcca722d52b02637930
Ptashkina Liliya <efekta9@gmail.com>
2019-02-27 11:20:46 +0300

    no message

--- other/my.py
+++ other/my.py
@@ -1,7 +1,7 @@
 def convert():
     try:
         c = input('Введите число: ')
-        return float(с)
+        return float('с')
     except ValueError:
         print('Не удалось преобразовать строку в число с плавающей точкой.')
     result = c
